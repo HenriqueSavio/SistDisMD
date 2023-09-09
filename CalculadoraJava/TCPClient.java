08976f6c85b58b349878f5286195bbed1e481b73
HenriqueSavio <henriquesaviots@gmail.com>
2023-09-08 23:18:07 -0300

    colocando threads no servidor e alterando algumas coisas no cliente

--- CalculadoraJava/TCPClient.java
+++ CalculadoraJava/TCPClient.java
@@ -1,22 +1,40 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class TCPClient {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         try {
             Socket socket = new Socket("localhost", 7896);
             
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            
-            String request = "mult,10,5";
+            while (true) {
+                System.out.println("Operação (add,sub,div,mult): ");
+                String operation = scanner.nextLine();
 
-            out.println(request);
+                if (operation.equalsIgnoreCase("exit")) {
+                    break;
+                }
             
-            String result = in.readLine();
-            System.out.println(result);
-            
+                System.out.println("Numero 1: ");
+                double num1 = Double.parseDouble(scanner.nextLine());
+
+                System.out.println("Numero 2: ");
+                double num2 = Double.parseDouble(scanner.nextLine());
+
+                String request = operation + "," + num1 + "," + num2;
+
+                out.println(request);
+                
+                String result = in.readLine();
+                System.out.println(result);
+            }
+
             socket.close();
+                
         } catch (IOException e) {
             e.printStackTrace();
         }
--- CalculadoraJava/TCPServer.java
+++ CalculadoraJava/TCPServer.java
@@ -1,32 +1,62 @@
 import java.io.*;
 import java.net.*;
+
 public class TCPServer {
-	    public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(7896);
-            
-            System.out.println("porta aberta 7896...");
-            
+            System.out.println("Porta aberta 7896...");
+
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
-                
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                
+
+                // Crie uma instância da classe Connection para lidar com o cliente
+                new Connection(clientSocket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Connection extends Thread {
+    BufferedReader in;
+    PrintWriter out;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("Conexão:" + e.getMessage());
+        }
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                // Read input from the client (request string)
                 String request = in.readLine();
+
+                if (request == null) {
+                    break; // Cliente fechou a conexão
+                }
 
                 try {
                     double result = Calculadora.handleRequest(request);
-                    out.println("Resultado: " + result);                        
-                } catch(IllegalArgumentException e){
+                    out.println("Result: " + result);
+                } catch (IllegalArgumentException e) {
                     out.println("Error: " + e.getMessage());
-                } catch (ArithmeticException e){
+                } catch (ArithmeticException e) {
                     out.println("Error: " + e.getMessage());
                 }
-                
-                clientSocket.close();
             }
+
+            // Feche o socket do cliente
+            clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
